59beca8d7c90418e00f1c3bb18afed0763aa23ce
Tom Palmer <remlapmot@hotmail.com>
2026-01-29 11:15:51 +0000

    Add sessionInfo() in tests

--- tests.sh
+++ tests.sh
@@ -1,2 +1,2 @@
-R -e "library(SEQTaRget); packageVersion('SEQTaRget'); quarto::quarto_version(); getOption('repos')"
+R -e "sessionInfo(); library(SEQTaRget); packageVersion('SEQTaRget'); quarto::quarto_version(); getOption('repos')"
 quarto --version
